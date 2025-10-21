b0b5bd4dcb399701b54a04f0a307fffc6b85ef4d
laowang <847063657@qq.com>
2025-10-21 18:07:56 +0800

    1111

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,40 @@
-import { defineConfig } from 'astro/config'
+import {defineConfig} from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import partytown from '@astrojs/partytown'
 import icon from 'astro-icon'
 import rehypeFigureTitle from 'rehype-figure-title'
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
-import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
-import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
+import {rehypeAccessibleEmojis} from 'rehype-accessible-emojis'
+import {remarkReadingTime} from './src/plugins/remark-reading-time.mjs'
+import {remarkModifiedTime} from './src/plugins/remark-modified-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yashjawale.github.io',
-	base: '/china-local-guide',
-	integrations: [
-		mdx(),
-		sitemap(),
-		icon(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	markdown: {
-		remarkPlugins: [remarkReadingTime, remarkModifiedTime],
-		rehypePlugins: [rehypeFigureTitle, rehypeAccessibleEmojis],
-	},
+    site: 'https://yashjawale.github.io',
+    base: '/china-local-guide',
+    optimize: {
+        css: false
+    },
+    integrations: [
+        mdx(),
+        sitemap(),
+        icon(),
+        partytown(
+            {
+                config: {
+                    forward: [
+                        'dataLayer.push'
+                    ],
+                },
+            }
+        ),
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    markdown: {
+        remarkPlugins: [remarkReadingTime, remarkModifiedTime],
+        rehypePlugins: [rehypeFigureTitle, rehypeAccessibleEmojis],
+    },
 })
