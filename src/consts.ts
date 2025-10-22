71e820b6b92673778195990323f932f5eb5af598
laowang <847063657@qq.com>
2025-10-22 17:48:12 +0800

    11111

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,9 @@
 
 export const SITE_TITLE = 'Ask China'
 export const SITE_DESCRIPTION = "Questions about China? Go to Ask China. Discover a real China with Ask China, a global Q&A community connecting you with the experts on China. "
-export const SITE_BASE = '111111111'
+
+// 这里是一级路由
+export const SITE_BASE = ''
 export const SocialLinks = [
     {
         name: 'LinkedIn',
