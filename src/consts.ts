a539482879eb6282b73624010c34656e03cc26ab
laowang <847063657@qq.com>
2025-10-22 17:36:22 +0800

    1

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'head 中的标题'
-export const SITE_DESCRIPTION = 'head 中的desc'
+export const SITE_TITLE = 'Ask China'
+export const SITE_DESCRIPTION = "Questions about China? Go to Ask China. Discover a real China with Ask China, a global Q&A community connecting you with the experts on China. "
 export const SITE_BASE = ''
 export const SocialLinks = [
-    // {
-    //     name: 'GitHub',
-    //     url: 'https://github.com/yashjawale',
-    // },
     {
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/yashjawale/',
@@ -27,7 +23,6 @@ export const WebsiteLinks = [
     {
         name: 'Ask China',
         url: 'ask-china',
-        // url: 'blog',
     },
     {
         name: 'About',
